caa84060dbb86dd55ec99cb6664b20ece209cdbf
Daniël Goossens <danielgoossens@outlook.com>
2019-09-15 15:50:11 +0200

    Fixed minor error in palette lore

--- src/main/java/nl/dgoossens/chiselsandbits2/common/items/PaletteItem.java
+++ src/main/java/nl/dgoossens/chiselsandbits2/common/items/PaletteItem.java
@@ -1,10 +1,12 @@
 package nl.dgoossens.chiselsandbits2.common.items;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
+import nl.dgoossens.chiselsandbits2.ChiselsAndBits2;
 import nl.dgoossens.chiselsandbits2.common.utils.ItemTooltipWriter;
 
 import javax.annotation.Nullable;
@@ -16,6 +18,8 @@ public class PaletteItem extends Item {
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
-        ItemTooltipWriter.addItemInformation(tooltip, "palette.help");
+        ItemTooltipWriter.addItemInformation(tooltip, "palette.help",
+                Minecraft.getInstance().gameSettings.keyBindUseItem,
+                ChiselsAndBits2.getKeybindings().modeMenu);
     }
 }
