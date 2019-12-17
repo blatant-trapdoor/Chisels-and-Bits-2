716da0ed42b5d1ce6c0e1bae64d62ddf4407f1e4
Daniël Goossens <daniel@goossens.ch>
2019-12-17 17:32:20 +0100

    Custom recipe didn't get registered properly

--- src/main/java/nl/dgoossens/chiselsandbits2/common/registry/ModRecipes.java
+++ src/main/java/nl/dgoossens/chiselsandbits2/common/registry/ModRecipes.java
@@ -3,14 +3,16 @@ package nl.dgoossens.chiselsandbits2.common.registry;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.SpecialRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
+import nl.dgoossens.chiselsandbits2.ChiselsAndBits2;
 import nl.dgoossens.chiselsandbits2.common.items.recipes.RecoloringRecipe;
 
 public class ModRecipes {
-    public final SpecialRecipeSerializer<RecoloringRecipe> CRAFTING_SPECIAL_RECOLOR = register(new SpecialRecipeSerializer<>(RecoloringRecipe::new));
+    public final SpecialRecipeSerializer<RecoloringRecipe> CRAFTING_SPECIAL_RECOLOR = register("crafting_special_recolor", new SpecialRecipeSerializer<>(RecoloringRecipe::new));
 
-    private static <S extends IRecipeSerializer<T>, T extends IRecipe<?>> S register(S recipeSerializer) {
-        ForgeRegistries.RECIPE_SERIALIZERS.register(recipeSerializer);
+    private static <S extends IRecipeSerializer<T>, T extends IRecipe<?>> S register(String key, S recipeSerializer) {
+        ForgeRegistries.RECIPE_SERIALIZERS.register(recipeSerializer.setRegistryName(new ResourceLocation(ChiselsAndBits2.MOD_ID, key)));
         return recipeSerializer;
     }
 }
