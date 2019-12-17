76e3ac43ef3f002120607c30462eb09acbdd7621
Daniël Goossens <daniel@goossens.ch>
2019-12-17 20:39:18 +0100

    Remove bit nudging modes

--- src/main/java/nl/dgoossens/chiselsandbits2/common/impl/ItemMode.java
+++ src/main/java/nl/dgoossens/chiselsandbits2/common/impl/ItemMode.java
@@ -36,8 +36,8 @@ public enum ItemMode implements ItemModeEnum {
     WRENCH_ROTATE,
     WRENCH_ROTATECCW,
     WRENCH_MIRROR,
-    WRENCH_NUDGE_BIT,
-    WRENCH_NUDGE_BLOCK,
+    //WRENCH_NUDGE_BIT,
+    //WRENCH_NUDGE_BLOCK,
 
     BLUEPRINT_UNKNOWN,
 
