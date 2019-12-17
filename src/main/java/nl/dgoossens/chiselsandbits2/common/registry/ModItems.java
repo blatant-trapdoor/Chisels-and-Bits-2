3a308da091f2377043d627cbbcbc93814c030694
Daniël <daniel@goossens.ch>
2019-12-17 14:33:15 +0100

    Fixed registration of IColourables

--- src/main/java/nl/dgoossens/chiselsandbits2/common/registry/ModItems.java
+++ src/main/java/nl/dgoossens/chiselsandbits2/common/registry/ModItems.java
@@ -21,16 +21,17 @@ import java.util.function.Supplier;
 public class ModItems {
     //Cached data to make colour lookups faster.
     private Map<Class<? extends IColourable>, Map<DyedItemColour, IColourable>> colourables = new HashMap<>();
+    private Map<Class<? extends IColourable>, String> colourableRegistryNames = new HashMap<>();
 
     public final Item CHISEL = new ChiselItem(new Item.Properties().maxDamage(-1).group(ModItemGroups.CHISELS_AND_BITS2));
     public Item PATTERN;
+    public Item PATTERN_BINDER;
     public Item SAW;
     public final Item TAPE_MEASURE = new TapeMeasureItem(new Item.Properties().maxStackSize(1).group(ModItemGroups.CHISELS_AND_BITS2));
     public Item WRENCH = new WrenchItem(new Item.Properties().maxDamage(1536).group(ModItemGroups.CHISELS_AND_BITS2));
-    public Item MALLET;
     public Item BLUEPRINT;
     public final Item MORPHING_BIT = new MorphingBitItem(new Item.Properties().group(ModItemGroups.CHISELS_AND_BITS2));
-    public Item PATTERN_BOOK;
+
     public final Item BIT_BAG = new BitBagItem();
     public Item BIT_BEAKER;
     
@@ -46,9 +47,8 @@ public class ModItems {
         if(ChiselsAndBits2.showUnfinishedFeatures()) {
             PATTERN = new PatternItem(new Item.Properties().maxStackSize(1).group(ModItemGroups.CHISELS_AND_BITS2));
             SAW = new SawItem(new Item.Properties().maxDamage(238).group(ModItemGroups.CHISELS_AND_BITS2));
-            MALLET = new MalletItem(new Item.Properties().maxDamage(238).group(ModItemGroups.CHISELS_AND_BITS2));
             BLUEPRINT = new BlueprintItem(new Item.Properties().maxStackSize(1).group(ModItemGroups.CHISELS_AND_BITS2));
-            PATTERN_BOOK = new Item(new Item.Properties().maxStackSize(1).group(ModItemGroups.CHISELS_AND_BITS2));
+            PATTERN_BINDER = new Item(new Item.Properties().maxStackSize(1).group(ModItemGroups.CHISELS_AND_BITS2));
             OAK_PALETTE = new PaletteItem();
             SPRUCE_PALETTE = new PaletteItem();
             BIRCH_PALETTE = new PaletteItem();
@@ -56,22 +56,24 @@ public class ModItems {
             ACACIA_PALETTE = new PaletteItem();
             DARK_OAK_PALETTE = new PaletteItem();
             BIT_BEAKER = new BitBeakerItem();
-            buildColourable(BitBeakerItem.class, BitBeakerItem::new);
+            buildColourable(BitBeakerItem.class, BitBeakerItem::new, "_TINTED_BIT_BEAKER");
         }
 
         //Build the colourables
-        buildColourable(BitBagItem.class, BitBagItem::new);
+        buildColourable(BitBagItem.class, BitBagItem::new, "_BIT_BAG");
     }
 
     /**
      * Builds a set of colourable items from a class that implements IColourable.
      * Automatically adds recipe.
+     * @param registryName Suffix to the registry name, will be prefixed with colour. E.g. _TINTED_BIT_BEAKER or _BIT_BAG, starting with _ is advised
      */
-    public <T extends IColourable> void buildColourable(Class<T> klass, Supplier<T> constructor) {
+    public <T extends IColourable> void buildColourable(Class<T> klass, Supplier<T> constructor, String registryName) {
         Map<DyedItemColour, IColourable> map = new HashMap<>();
         for(DyedItemColour c : DyedItemColour.values())
             map.put(c, constructor.get());
         colourables.put(klass, map);
+        colourableRegistryNames.put(klass, registryName);
     }
     
     /**
@@ -104,6 +106,7 @@ public class ModItems {
      * Internal method used by recipe generators.
      */
     public <T extends Item> T getColouredItem(final Class<T> klass, final DyedItemColour colour) {
+        if(!(Item.class.isAssignableFrom(klass))) throw new UnsupportedOperationException("An IColourable must also extend Item!");
         return (T) colourables.getOrDefault(klass, new HashMap<>()).get(colour);
     }
 
@@ -141,5 +144,13 @@ public class ModItems {
     public static void onItemRegistry(final RegistryEvent.Register<Item> e) {
         //Register all items in this class automatically.
         registerAll(e, ChiselsAndBits2.getInstance().getItems(), Item.class);
+
+        //Register IColourables
+        for(Map.Entry<Class<? extends IColourable>, Map<DyedItemColour, IColourable>> r : ChiselsAndBits2.getInstance().getItems().colourables.entrySet()) {
+            for(Map.Entry<DyedItemColour, IColourable> en : r.getValue().entrySet()) {
+                Item i = (Item) en.getValue();
+                e.getRegistry().register(i.setRegistryName(ChiselsAndBits2.MOD_ID, en.getKey().name()+ChiselsAndBits2.getInstance().getItems().colourableRegistryNames.get(r.getKey()).toLowerCase()));
+            }
+        }
     }
 }
